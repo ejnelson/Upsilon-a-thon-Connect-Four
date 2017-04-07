11c4a1ebe49ca5bf4b431b8045d6e04546e7db4b
Bob-Hinkley <bob.hinkley1@gmail.com>
2017-04-07 15:22:47 -0500

    Re-added server connection.  DON'T DELETE IT THIS TIMEgit status

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 
 exports.connect = function () {
-    mongoose.connect(process.env.MONGODB_URI); //CHANGE THIS BOB.... I THINK
+    // mongoose.connect(process.env.MONGODB_URI); //CHANGE THIS BOB.... I
+    mongoose.connect('mongodb://localhost:27017/connect-four');
 
   mongoose.connection.on('error', function(error){
     console.log('error connecting', error);
