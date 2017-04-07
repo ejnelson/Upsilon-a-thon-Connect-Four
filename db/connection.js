8f059689524470c9cb7412e4a7e20d5b6f9ef46f
ejnelson <erik@ejnelson.com>
2017-04-07 10:54:49 -0500

    delete a bunch of stuff

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 exports.connect = function () {
-    mongoose.connect(process.env.MONGODB_URI);
+    mongoose.connect(process.env.MONGODB_URI); //CHANGE THIS BOB.... I THINK
 
   mongoose.connection.on('error', function(error){
     console.log('error connecting', error);
--- public/scripts/uploadService.js
+++
@@ -1,8 +0,0 @@
-angular.module('myApp').service('uploadService', function ($http,$location) {
-
-      console.log('upload service loaded');
-
-
-
-
-});
